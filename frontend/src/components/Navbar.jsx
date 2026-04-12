96161f02c03f2e0c613999cb120e1d4ffb357716
Saurabh-LTCE <saurabhsharma13052004@gmail.com>
2026-04-12 17:18:00 +0530

    updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,21 @@
+import dumbelIcon from "../assets/icons/Dumbelicon.png";
 import React from 'react';
-import { IconGym } from './Icons';
 
 const Navbar = () => (
   <header className="navbar">
     <div className="navbar__inner">
       <div className="navbar__brand">
-        <span className="navbar__icon-wrap" aria-hidden>
-          <IconGym className="navbar__icon" />
+        
+        <span className="w-12 h-12 flex items-center justify-center border-2 border-black bg-white shadow-[4px_4px_0px_black]">
+          <img 
+            src={dumbelIcon} 
+            alt="gym icon"
+            className="navbar__icon"
+          />
         </span>
+
         <h1 className="navbar__title">Gym Management</h1>
+
       </div>
     </div>
   </header>
